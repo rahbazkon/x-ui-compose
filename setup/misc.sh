8032947c5147fc50ac7b35139bfbbb4c57e34b48
rahbazkon <rahbazkonus@proton.me>
2023-01-21 22:28:02 +0330

    feat: added git status alias

--- setup/misc.sh
+++ setup/misc.sh
@@ -4,6 +4,7 @@
 printf "Adding proper aliases to .zshrc\n"
 # shellcheck disable=SC2129
 echo "alias c='clear'" >> "$HOME"/.zshrc
+echo "alias gs='git status'" >> "$HOME"/.zshrc
 echo "alias upd='sudo apt update'" >> "$HOME"/.zshrc
 echo "alias upg='sudo apt full-upgrade'" >> "$HOME"/.zshrc
 echo "alias dc='docker compose'" >> "$HOME"/.zshrc
