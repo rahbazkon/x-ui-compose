e1f2a45b2d2f4e3068d7dd4c41686eab962bd6c3
rahbazkon <rahbazkonus@proton.me>
2023-06-03 00:56:50 +0330

    fix

--- uptime-kuma/cronvmesstest.sh
+++ uptime-kuma/cronvmesstest.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+
+# */15 * * * * /root/x-ui-compose/uptime-kuma/cronvmesstest.sh >/dev/null 2>&1
+
 vmess="$1"
 uptime_kuma_base_api_url="$2"
 ping_count=10
 ping_delay=5
-api_interval=900
 down_message="NotOK"
 up_message="OK"
 
-avg_ping_time=$(./vmessping_amd64_linux -c $ping_count -i $ping_delay $vmess | grep "rtt min/avg/max" | awk -F '/' '{print $4}')
+avg_ping_time=$(./bin/vmessping_amd64_linux -c $ping_count -i $ping_delay $vmess | grep "rtt min/avg/max" | awk -F '/' '{print $4}')
 
 if [ $avg_ping_time -eq 0 ]
 then
